9b5dff0ba10d14808b5997c6dda2a742093c6630
Jinzhu <wosmvp@gmail.com>
2015-12-03 16:24:10 +0800

    Refactor media library callbacks to supprot serializable_meta

--- callback.go
+++ callback.go
@@ -6,64 +6,79 @@ import (
 	"mime/multipart"
 
 	"github.com/jinzhu/gorm"
+	"github.com/qor/serializable_meta"
 )
+
+func cropField(field *gorm.Field, scope *gorm.Scope) (cropped bool) {
+	if media, ok := field.Field.Addr().Interface().(MediaLibrary); ok && !media.Cropped() {
+		option := parseTagOption(field.Tag.Get("media_library"))
+		if media.GetFileHeader() != nil || media.NeedCrop() {
+			var file multipart.File
+			var err error
+			if fileHeader := media.GetFileHeader(); fileHeader != nil {
+				file, err = media.GetFileHeader().Open()
+			} else {
+				file, err = media.Retrieve(media.URL("original"))
+			}
+
+			if err != nil {
+				scope.Err(err)
+				return false
+			}
+
+			media.Cropped(true)
+
+			if url := media.GetURL(option, scope, field, media); url == "" {
+				scope.Err(errors.New("invalid URL"))
+			} else {
+				result, _ := json.Marshal(map[string]string{"Url": url})
+				media.Scan(string(result))
+			}
+
+			if file != nil {
+				defer file.Close()
+				var handled = false
+				for _, handler := range mediaLibraryHandlers {
+					if handler.CouldHandle(media) {
+						if scope.Err(handler.Handle(media, file, option)) == nil {
+							handled = true
+						}
+					}
+				}
+
+				// Save File
+				if !handled {
+					scope.Err(media.Store(media.URL(), option, file))
+				}
+			}
+			return true
+		}
+	}
+	return false
+}
 
 func SaveAndCropImage(isCreate bool) func(scope *gorm.Scope) {
 	return func(scope *gorm.Scope) {
 		if !scope.HasError() {
 			var updateColumns = map[string]interface{}{}
 			for _, field := range scope.Fields() {
-				if media, ok := field.Field.Addr().Interface().(MediaLibrary); ok && !media.Cropped() {
-
-					option := parseTagOption(field.Tag.Get("media_library"))
-					if media.GetFileHeader() != nil || media.NeedCrop() {
-						var file multipart.File
-						var err error
-						if fileHeader := media.GetFileHeader(); fileHeader != nil {
-							file, err = media.GetFileHeader().Open()
-						} else {
-							file, err = media.Retrieve(media.URL("original"))
-						}
-
-						if scope.Err(err) != nil {
-							return
-						}
-						media.Cropped(true)
-
-						if url := media.GetURL(option, scope, field, media); url == "" {
-							scope.Err(errors.New("invalid URL"))
-						} else {
-							result, _ := json.Marshal(map[string]string{"Url": url})
-							media.Scan(string(result))
-						}
-
-						if isCreate {
-							if value, err := media.Value(); err == nil {
-								updateColumns[field.DBName] = value
-							}
-						}
-
-						if file != nil {
-							defer file.Close()
-							var handled = false
-							for _, handler := range mediaLibraryHandlers {
-								if handler.CouldHandle(media) {
-									if scope.Err(handler.Handle(media, file, option)) == nil {
-										handled = true
-									}
-								}
-							}
-
-							// Save File
-							if !handled {
-								scope.Err(media.Store(media.URL(), option, file))
-							}
+				if value, ok := field.Field.Interface().(serializable_meta.SerializableMetaInterface); ok {
+					var croppedField bool
+					for _, field := range scope.New(value.GetSerializableArgument(value)).Fields() {
+						if cropField(field, scope) {
+							croppedField = true
 						}
 					}
+
+					if croppedField && isCreate {
+						updateColumns[field.DBName] = field.Field.Interface()
+					}
+				} else if cropField(field, scope) && isCreate {
+					updateColumns[field.DBName] = field.Field.Interface()
 				}
 			}
 
-			if isCreate && !scope.HasError() && len(updateColumns) != 0 {
+			if !scope.HasError() && len(updateColumns) != 0 {
 				scope.NewDB().Model(scope.Value).UpdateColumns(updateColumns)
 			}
 		}
