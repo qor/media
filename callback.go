baca153cd0f0358991aa06087c65556b33ba640d
Jinzhu <wosmvp@gmail.com>
2015-12-28 12:12:56 +0800

    Fix unaddressable field exception

--- callback.go
+++ callback.go
@@ -10,48 +10,50 @@ import (
 )
 
 func cropField(field *gorm.Field, scope *gorm.Scope) (cropped bool) {
-	if media, ok := field.Field.Addr().Interface().(MediaLibrary); ok && !media.Cropped() {
-		option := parseTagOption(field.Tag.Get("media_library"))
-		if media.GetFileHeader() != nil || media.NeedCrop() {
-			var file multipart.File
-			var err error
-			if fileHeader := media.GetFileHeader(); fileHeader != nil {
-				file, err = media.GetFileHeader().Open()
-			} else {
-				file, err = media.Retrieve(media.URL("original"))
-			}
+	if field.Field.CanAddr() {
+		if media, ok := field.Field.Addr().Interface().(MediaLibrary); ok && !media.Cropped() {
+			option := parseTagOption(field.Tag.Get("media_library"))
+			if media.GetFileHeader() != nil || media.NeedCrop() {
+				var file multipart.File
+				var err error
+				if fileHeader := media.GetFileHeader(); fileHeader != nil {
+					file, err = media.GetFileHeader().Open()
+				} else {
+					file, err = media.Retrieve(media.URL("original"))
+				}
 
-			if err != nil {
-				scope.Err(err)
-				return false
-			}
+				if err != nil {
+					scope.Err(err)
+					return false
+				}
 
-			media.Cropped(true)
+				media.Cropped(true)
 
-			if url := media.GetURL(option, scope, field, media); url == "" {
-				scope.Err(errors.New("invalid URL"))
-			} else {
-				result, _ := json.Marshal(map[string]string{"Url": url})
-				media.Scan(string(result))
-			}
+				if url := media.GetURL(option, scope, field, media); url == "" {
+					scope.Err(errors.New("invalid URL"))
+				} else {
+					result, _ := json.Marshal(map[string]string{"Url": url})
+					media.Scan(string(result))
+				}
 
-			if file != nil {
-				defer file.Close()
-				var handled = false
-				for _, handler := range mediaLibraryHandlers {
-					if handler.CouldHandle(media) {
-						if scope.Err(handler.Handle(media, file, option)) == nil {
-							handled = true
+				if file != nil {
+					defer file.Close()
+					var handled = false
+					for _, handler := range mediaLibraryHandlers {
+						if handler.CouldHandle(media) {
+							if scope.Err(handler.Handle(media, file, option)) == nil {
+								handled = true
+							}
 						}
 					}
-				}
 
-				// Save File
-				if !handled {
-					scope.Err(media.Store(media.URL(), option, file))
+					// Save File
+					if !handled {
+						scope.Err(media.Store(media.URL(), option, file))
+					}
 				}
+				return true
 			}
-			return true
 		}
 	}
 	return false
