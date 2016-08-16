96997ae3537cedf00678b69eab504aabd1795910
Jinzhu <wosmvp@gmail.com>
2016-08-16 11:51:23 +0800

    Add media box

---
+++ views/assets/javascripts/media_box/media_box.js
@@ -0,0 +1,16 @@
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node / CommonJS
+    factory(require('jquery'));
+  } else {
+    // Browser globals.
+    factory(jQuery);
+  }
+})(function ($) {
+
+  'use strict';
+
+});
