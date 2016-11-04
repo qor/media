9cdeb90c321f481b9bf02d4222b36dc8d7053c16
Azuma Fan <azuma@theplant.jp>
2016-11-04 15:39:20 +0800

    support aws ec2 role credentials

--- aws/s3.go
+++ aws/s3.go
@@ -9,9 +9,12 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/qor/media_library"
@@ -41,16 +44,35 @@ var awsSessionToken = os.Getenv("QOR_AWS_SESSION_TOKEN")
 
 func s3client() *s3.S3 {
 	if client == nil {
-		creds := credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, awsSessionToken)
-
-		if _, err := creds.Get(); err == nil {
-			client = s3.New(session.New(), &aws.Config{
-				Region:      &awsRegion,
-				Credentials: creds,
-			})
+		var creds *credentials.Credentials
+		if awsAccessKeyID == "" && awsSecretAccessKey == "" {
+			client = s3.New(session.New(), EC2RoleAwsConfig())
+		} else {
+			creds = credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, awsSessionToken)
+			if _, err := creds.Get(); err == nil {
+				client = s3.New(session.New(), &aws.Config{
+					Region:      &awsRegion,
+					Credentials: creds,
+				})
+			}
 		}
 	}
 	return client
+}
+
+func EC2RoleAwsConfig() *aws.Config {
+	ec2m := ec2metadata.New(session.New(), &aws.Config{
+		HTTPClient: &http.Client{Timeout: 10 * time.Second},
+		Endpoint:   aws.String("http://169.254.169.254/latest"),
+	})
+	cr := credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{
+		Client: ec2m,
+	})
+
+	return &aws.Config{
+		Region:      &awsRegion,
+		Credentials: cr,
+	}
 }
 
 func getBucket(option *media_library.Option) string {
