580abec2f6d58c180a1b1a8794817d3770c5d32b
Azuma Fan <azuma@theplant.jp>
2016-11-04 16:11:57 +0800

    cloudfront backward compatibility to s3 when dont set QOR_AWS_CLOUD_FRONT_DOMAIN

--- aws/cloudfront.go
+++ aws/cloudfront.go
@@ -27,8 +27,11 @@ func (s CloudFront) GetURLTemplate(option *media_library.Option) (path string) {
 	if path = option.Get("URL"); path == "" {
 		path = "/{{class}}/{{primary_key}}/{{column}}/{{filename_with_hash}}"
 	}
+	if awsCloudFontDomain != "" {
+		return awsCloudFontDomain + path
+	}
+	return "//" + getEndpoint(option) + path
 
-	return awsCloudFontDomain + path
 }
 
 // Store store reader's content with url
@@ -43,7 +46,16 @@ func (s CloudFront) Store(url string, option *media_library.Option, reader io.Re
 	fileBytesLen := int64(fileBytes.Len())
 
 	// Retrieve the file path from the file URL.
-	filePath := strings.TrimPrefix(url, awsCloudFontDomain)
+	filePath := url
+	acl := s3.ObjectCannedACLPrivate
+	if awsCloudFontDomain != "" {
+		filePath = strings.TrimPrefix(url, awsCloudFontDomain)
+	} else {
+		acl = s3.ObjectCannedACLPublicRead
+		for _, scheme := range []string{"https://", "http://", "//"} {
+			filePath = strings.TrimPrefix(filePath, scheme+getEndpoint(option))
+		}
+	}
 
 	fileType := mime.TypeByExtension(path.Ext(filePath))
 	if fileType == "" {
@@ -53,7 +65,7 @@ func (s CloudFront) Store(url string, option *media_library.Option, reader io.Re
 	params := &s3.PutObjectInput{
 		Bucket:        aws.String(getBucket(option)), // required
 		Key:           aws.String(filePath),          // required
-		ACL:           aws.String("private"),
+		ACL:           aws.String(acl),
 		Body:          fileBytes,
 		ContentLength: &fileBytesLen,
 		ContentType:   aws.String(fileType),
@@ -63,19 +75,4 @@ func (s CloudFront) Store(url string, option *media_library.Option, reader io.Re
 	}
 	_, err = s3client().PutObject(params)
 	return err
-}
-
-// Retrieve retrieve file content with url
-func (s CloudFront) Retrieve(url string) (file *os.File, err error) {
-	response, err := http.Get("http:" + url)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	if file, err = ioutil.TempFile("/tmp", "s3"); err == nil {
-		_, err := io.Copy(file, response.Body)
-		return file, err
-	}
-	return nil, err
 }
