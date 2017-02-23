016fae705b1742d0239acc6fa8fbe0dcc53c8082
Jinzhu <wosmvp@gmail.com>
2017-02-23 22:03:52 +0800

    Allow customize OSS's URLTemplate, Store, Retrieve

--- oss/oss.go
+++ oss/oss.go
@@ -13,8 +13,9 @@ import (
 
 var (
 	// Storage the storage used to save medias
-	Storage     oss.StorageInterface = filesystem.New("public")
-	URLTemplate                      = "/system/{{class}}/{{primary_key}}/{{column}}/{{filename_with_hash}}"
+	Storage oss.StorageInterface = filesystem.New("public")
+	// URLTemplate default URL template
+	URLTemplate = "/system/{{class}}/{{primary_key}}/{{column}}/{{filename_with_hash}}"
 )
 
 // OSS common storage interface
@@ -22,8 +23,8 @@ type OSS struct {
 	media_library.Base
 }
 
-// GetURLTemplate URL's template
-func (OSS) GetURLTemplate(option *media_library.Option) (url string) {
+// DefaultURLTemplateHandler used to generate URL and save into database
+var DefaultURLTemplateHandler = func(option *media_library.Option) (url string) {
 	if url = option.Get("URL"); url == "" {
 		url = URLTemplate
 	}
@@ -41,13 +42,28 @@ func (OSS) GetURLTemplate(option *media_library.Option) (url string) {
 	return "//" + url
 }
 
-// Store save reader's content with path
-func (OSS) Store(path string, option *media_library.Option, reader io.Reader) error {
+// GetURLTemplate URL's template
+func (OSS) GetURLTemplate(option *media_library.Option) (url string) {
+	return DefaultURLTemplateHandler(option)
+}
+
+// DefaultStoreHandler used to store reader with default Storage
+var DefaultStoreHandler = func(path string, option *media_library.Option, reader io.Reader) error {
 	_, err := Storage.Put(path, reader)
 	return err
 }
 
+// Store save reader's content with path
+func (OSS) Store(path string, option *media_library.Option, reader io.Reader) error {
+	return DefaultStoreHandler(path, option, reader)
+}
+
+// DefaultRetrieveHandler used to retrieve file
+var DefaultRetrieveHandler = func(path string) (*os.File, error) {
+	return Storage.Get(path)
+}
+
 // Retrieve retrieve file content with url
 func (OSS) Retrieve(path string) (*os.File, error) {
-	return Storage.Get(path)
+	return DefaultRetrieveHandler(path)
 }
