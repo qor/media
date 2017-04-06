d6a534c8606eb5949f51571d794521ba8d53603a
Jinzhu <wosmvp@gmail.com>
2017-04-06 15:00:35 +0800

    Add IsImage, IsVideo method for File

--- media_library/media_library.go
+++ media_library/media_library.go
@@ -334,6 +334,15 @@ type File struct {
 	Description string
 }
 
+// IsImage return if it is an image
+func (f File) IsImage() bool {
+	return media.IsImageFormat(f.Url)
+}
+
+func (f File) IsVideo() bool {
+	return media.IsVideoFormat(f.Url)
+}
+
 func (file File) URL(styles ...string) string {
 	if file.Url != "" && len(styles) > 0 {
 		ext := path.Ext(file.Url)
