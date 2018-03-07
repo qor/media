912a5c84dbc4e5f30b9edbe3a3b125280b9ad4fb
jian weng <wengjianm@gmail.com>
2018-03-07 12:02:07 +0800

    Fix className display bug

--- views/assets/javascripts/qor_redactor_media_display_mode/qor_redactor_media_display_mode.js
+++ views/assets/javascripts/qor_redactor_media_display_mode/qor_redactor_media_display_mode.js
@@ -35,7 +35,7 @@ $(function() {
                         <section>
                             <select id="modal-media-display-mode">
                                 <option value="0">Please select display mode</option>
-                                <option value="half-container">Half Container Width</option>
+                                <option value="halfcontainer">Half Container Width</option>
                                 <option value="container">Container Width</option>
                                 <option value="fullwidth">Full Width</option>
                             </select>
