36129146227d1cb66deecab8741dc75a2caaf62d
wtser <wangtieshou@gmail.com>
2018-12-13 17:55:08 +0800

    Update medialib plugin

--- views/assets/javascripts/qor_redactor_medialibrary/qor_redactor_medialibrary.js
+++ views/assets/javascripts/qor_redactor_medialibrary/qor_redactor_medialibrary.js
@@ -2,190 +2,248 @@
 // By Jason weng @theplant
 //
 
-$.Redactor.prototype.medialibrary = function() {
-    return {
-        init: function() {
-            var button = this.button.add('medialibrary', 'MediaLibrary');
-            this.button.addCallback(button, this.medialibrary.addMedialibrary);
-            this.button.setIcon(button, '<i class="material-icons">photo_library</i>');
-            $(document).on('reload.qor.bottomsheets', '.qor-bottomsheets__mediabox', this.medialibrary.initItem);
-        },
+(function($R) {
+  $R.add("plugin", "medialibrary", {
+    init: function(app) {
+      this.app = app;
+      this.opts = app.opts;
+      this.lang = app.lang;
+      this.inline = app.inline;
+      this.toolbar = app.toolbar;
+      this.func = new this.funcInit(this);
+    },
 
-        addMedialibrary: function() {
-            var $element = this.$element,
-                data = {selectModal: 'mediabox', maxItem: '1'},
-                mediaboxUrl = $element.data().redactorSettings.medialibraryUrl,
-                BottomSheets;
+    start: function() {
+      var $button = this.toolbar.addButton("medialibrary", {
+        title: "MediaLibrary",
+        api: "plugin.medialibrary.addMedialibrary"
+      });
+      $button.setIcon('<i class="material-icons">photo_library</i>');
+      this.buttonElement = $button.nodes[0];
 
-            this.medialibrary.BottomSheets = BottomSheets = $('body').data('qor.bottomsheets');
-            data.url = mediaboxUrl;
-            BottomSheets.open(data, this.medialibrary.handleMediaLibrary);
-        },
+      $(document).on(
+        "reload.qor.bottomsheets",
+        ".qor-bottomsheets__mediabox",
+        this.func.initItem
+      );
+    },
 
-        handleMediaLibrary: function($bottomsheets) {
-            var options = {
-                onSelect: this.medialibrary.handleResults, // render selected item after click item lists
-                onSubmit: this.medialibrary.handleResults // render new items after new item form submitted
-            };
+    addMedialibrary: function() {
+      this.func.addMedialibrary();
+    },
 
-            this.medialibrary.$bottomsheets = $bottomsheets;
-            $bottomsheets.qorSelectCore(options).addClass('qor-bottomsheets__mediabox');
-            this.medialibrary.initItem();
-        },
+    funcInit: function(self) {
+      var thisApp = self;
+      var thisFunc = this;
+      this.addMedialibrary = function() {
+        var $element = $(thisApp.app.rootElement),
+          data = { selectModal: "mediabox", maxItem: "1" },
+          mediaboxUrl = $element.data().redactorSettings.medialibraryUrl,
+          BottomSheets;
 
-        initItem: function() {
-            var $trs = $('.qor-bottomsheets').find('tbody tr'),
-                $tr,
-                $img;
+        thisFunc.BottomSheets = BottomSheets = $("body").data(
+          "qor.bottomsheets"
+        );
+        data.url = mediaboxUrl;
+        BottomSheets.open(data, function($bottomsheets) {
+          thisFunc.handleMediaLibrary($bottomsheets);
+        });
+      };
 
-            $trs.each(function() {
-                $tr = $(this);
-                $img = $tr.find('.qor-table--ml-slideout p img').first();
-                $tr.find('.qor-table__actions').remove();
-                if ($img.length) {
-                    $tr.find('.qor-table--medialibrary-item').css('background-image', 'url(' + $img.prop('src') + ')');
-                    $img.parent().remove();
-                }
-            });
-        },
+      this.handleMediaLibrary = function($bottomsheets) {
+        let options = {
+          onSelect: thisFunc.handleResults, // render selected item after click item lists
+          onSubmit: thisFunc.handleResults // render new items after new item form submitted
+        };
 
-        handleResults: function(data) {
-            var reVideo = /\.mp4$|\.m4p$|\.m4v$|\.m4v$|\.mov$|\.mpeg$|\.webm$|\.avi$|\.ogg$|\.ogv$/,
-                mediaOption = data.MediaOption;
+        thisFunc.$bottomsheets = $bottomsheets;
+        $bottomsheets
+          .qorSelectCore(options)
+          .addClass("qor-bottomsheets__mediabox");
+        thisFunc.initItem();
+      };
 
-            if (data.SelectedType == 'video_link' || mediaOption.Video || mediaOption.URL.match(reVideo)) {
-                this.medialibrary.insertVideo(data);
-            } else {
-                this.medialibrary.insertImage(data);
-            }
+      this.initItem = function() {
+        var $trs = $(".qor-bottomsheets").find("tbody tr"),
+          $tr,
+          $img;
 
-            this.medialibrary.$bottomsheets.remove();
-            if (!$('.qor-bottomsheets').is(':visible')) {
-                $('body').removeClass('qor-bottomsheets-open');
-            }
-        },
+        $trs.each(function() {
+          $tr = $(this);
+          $img = $tr.find(".qor-table--ml-slideout p img").first();
+          $tr.find(".qor-table__actions").remove();
+          if ($img.length) {
+            $tr
+              .find(".qor-table--medialibrary-item")
+              .css("background-image", "url(" + $img.prop("src") + ")");
+            $img.parent().remove();
+          }
+        });
+      };
 
-        insertVideo: function(data) {
-            this.opts.mediaContainerClass = typeof this.opts.mediaContainerClass === 'undefined' ? 'qor-video-container' : this.opts.mediaContainerClass;
+      this.handleResults = function(data) {
+        var reVideo = /\.mp4$|\.m4p$|\.m4v$|\.m4v$|\.mov$|\.mpeg$|\.webm$|\.avi$|\.ogg$|\.ogv$/,
+          mediaOption = data.MediaOption;
 
-            var htmlCode,
-                videoLink,
-                iframeStart,
-                iframeEnd,
-                videoType,
-                $html,
-                youkuID,
-                callbackData = {},
-                mediaContainerClass = this.opts.mediaContainerClass,
-                reUrlYoutube = this.opts.regexps.linkyoutube,
-                reUrlVimeo = this.opts.regexps.linkvimeo,
-                reUrlYouku = /http?:\/\/(www\.)|(v\.)youku.com/,
-                reUrlYoukuID = /(\/id_)(\w+)/,
-                $currentTag = this.selection.$currentTag,
-                reVideo = /\.mp4$|\.m4p$|\.m4v$|\.m4v$|\.mov$|\.mpeg$|\.webm$|\.avi$|\.ogg$|\.ogv$/,
-                randomString = (Math.random() + 1).toString(36).substring(7),
-                videoIdentification = 'qor-video-' + randomString,
-                mediaOption = data.MediaOption,
-                description = mediaOption.Description;
-
-            iframeStart = '<figure class="' + mediaContainerClass + '"><iframe title="' + description + '" width="100%" height="380px" src="';
-            iframeEnd = '" frameborder="0" allowfullscreen></iframe><figcaption>' + description + '</figcaption></figure>';
-
-            if (data.SelectedType == 'video_link') {
-                videoLink = mediaOption.Video;
-
-                if (videoLink.match(reUrlYoutube)) {
-                    videoType = 'youtube';
-                    htmlCode = videoLink.replace(reUrlYoutube, iframeStart + '//www.youtube.com/embed/$1' + iframeEnd);
-                }
-
-                if (videoLink.match(reUrlVimeo)) {
-                    videoType = 'vimeo';
-                    htmlCode = videoLink.replace(reUrlVimeo, iframeStart + '//player.vimeo.com/video/$2' + iframeEnd);
-                }
-
-                if (videoLink.match(reUrlYouku) && reUrlYoukuID.test(videoLink)) {
-                    videoType = 'youku';
-                    youkuID = videoLink.match(reUrlYoukuID)[2];
-                    htmlCode = '<iframe width=100% height=400 src="http://player.youku.com/embed/' + youkuID + '" frameborder=0 "allowfullscreen"></iframe>';
-                }
-            } else if (mediaOption.URL.match(reVideo)) {
-                videoType = 'uploadedVideo';
-                htmlCode =
-                    '<figure class="' +
-                    mediaContainerClass +
-                    '"><div role="application"><video width="100%" title="' +
-                    description +
-                    '" aria-label="' +
-                    description +
-                    '" height="380px" controls="controls" aria-describedby="' +
-                    videoIdentification +
-                    '" tabindex="0"><source src="' +
-                    mediaOption.URL +
-                    '"></video></div><figcaption id="' +
-                    videoIdentification +
-                    '">' +
-                    description +
-                    '</figcaption></figure>';
-            }
-
-            if (!htmlCode) {
-                return;
-            }
-
-            $html = $(htmlCode).addClass(videoIdentification);
-
-            if ($currentTag) {
-                $currentTag.after($html);
-            } else {
-                this.$editor.prepend($html);
-            }
-
-            this.code.sync();
-
-            // trigger insertedVideo.redactor event after inserted videos
-            callbackData.type = videoType;
-            callbackData.videoLink = videoLink || mediaOption.URL;
-            callbackData.videoIdentification = videoIdentification;
-            callbackData.description = description;
-            callbackData.$editor = this.$editor;
-            callbackData.$element = this.$element;
-
-            this.$element.trigger('insertedVideo.redactor', [callbackData]);
-        },
-
-        insertImage: function(data) {
-            var src,
-                $currentTag = this.selection.$currentTag,
-                $img = $('<img>'),
-                $figure = $('<' + this.opts.imageTag + '>'),
-                mediaOption = data.MediaOption,
-                callbackData = {};
-
-            src = mediaOption.URL.replace(/image\..+\./, 'image.');
-
-            $img.attr({
-                src: src,
-                alt: mediaOption.Description
-            });
-            $figure.append($img);
-
-            if ($currentTag) {
-                $currentTag.after($figure);
-            } else {
-                this.$editor.prepend($figure);
-            }
-
-            this.image.setEditable($img);
-            this.code.sync();
-
-            // trigger insertedVideo.redactor event after inserted images
-            callbackData.description = mediaOption.Description;
-            callbackData.$img = $figure;
-            callbackData.$editor = this.$editor;
-            callbackData.$element = this.$element;
-
-            this.$element.trigger('insertedImage.redactor', [callbackData]);
+        if (
+          data.SelectedType == "video_link" ||
+          mediaOption.Video ||
+          mediaOption.URL.match(reVideo)
+        ) {
+          thisFunc.insertVideo(data);
+        } else {
+          thisFunc.insertImage(data);
         }
-    };
-};
+
+        thisFunc.$bottomsheets.remove();
+        if (!$(".qor-bottomsheets").is(":visible")) {
+          $("body").removeClass("qor-bottomsheets-open");
+        }
+      };
+
+      this.insertVideo = function(data) {
+        this.opts.mediaContainerClass =
+          typeof this.opts.mediaContainerClass === "undefined"
+            ? "qor-video-container"
+            : this.opts.mediaContainerClass;
+
+        var htmlCode,
+          videoLink,
+          iframeStart,
+          iframeEnd,
+          videoType,
+          $html,
+          youkuID,
+          callbackData = {},
+          mediaContainerClass = this.opts.mediaContainerClass,
+          reUrlYoutube = this.opts.regexps.linkyoutube,
+          reUrlVimeo = this.opts.regexps.linkvimeo,
+          reUrlYouku = /http?:\/\/(www\.)|(v\.)youku.com/,
+          reUrlYoukuID = /(\/id_)(\w+)/,
+          $currentTag = this.selection.$currentTag,
+          reVideo = /\.mp4$|\.m4p$|\.m4v$|\.m4v$|\.mov$|\.mpeg$|\.webm$|\.avi$|\.ogg$|\.ogv$/,
+          randomString = (Math.random() + 1).toString(36).substring(7),
+          videoIdentification = "qor-video-" + randomString,
+          mediaOption = data.MediaOption,
+          description = mediaOption.Description;
+
+        iframeStart =
+          '<figure class="' +
+          mediaContainerClass +
+          '"><iframe title="' +
+          description +
+          '" width="100%" height="380px" src="';
+        iframeEnd =
+          '" frameborder="0" allowfullscreen></iframe><figcaption>' +
+          description +
+          "</figcaption></figure>";
+
+        if (data.SelectedType == "video_link") {
+          videoLink = mediaOption.Video;
+
+          if (videoLink.match(reUrlYoutube)) {
+            videoType = "youtube";
+            htmlCode = videoLink.replace(
+              reUrlYoutube,
+              iframeStart + "//www.youtube.com/embed/$1" + iframeEnd
+            );
+          }
+
+          if (videoLink.match(reUrlVimeo)) {
+            videoType = "vimeo";
+            htmlCode = videoLink.replace(
+              reUrlVimeo,
+              iframeStart + "//player.vimeo.com/video/$2" + iframeEnd
+            );
+          }
+
+          if (videoLink.match(reUrlYouku) && reUrlYoukuID.test(videoLink)) {
+            videoType = "youku";
+            youkuID = videoLink.match(reUrlYoukuID)[2];
+            htmlCode =
+              '<iframe width=100% height=400 src="http://player.youku.com/embed/' +
+              youkuID +
+              '" frameborder=0 "allowfullscreen"></iframe>';
+          }
+        } else if (mediaOption.URL.match(reVideo)) {
+          videoType = "uploadedVideo";
+          htmlCode =
+            '<figure class="' +
+            mediaContainerClass +
+            '"><div role="application"><video width="100%" title="' +
+            description +
+            '" aria-label="' +
+            description +
+            '" height="380px" controls="controls" aria-describedby="' +
+            videoIdentification +
+            '" tabindex="0"><source src="' +
+            mediaOption.URL +
+            '"></video></div><figcaption id="' +
+            videoIdentification +
+            '">' +
+            description +
+            "</figcaption></figure>";
+        }
+
+        if (!htmlCode) {
+          return;
+        }
+
+        $html = $(htmlCode).addClass(videoIdentification);
+
+        if ($currentTag) {
+          $currentTag.after($html);
+        } else {
+          thisApp.$editor.prepend($html);
+        }
+
+        this.code.sync();
+
+        // trigger insertedVideo.redactor event after inserted videos
+        callbackData.type = videoType;
+        callbackData.videoLink = videoLink || mediaOption.URL;
+        callbackData.videoIdentification = videoIdentification;
+        callbackData.description = description;
+        callbackData.$editor = this.$editor;
+        callbackData.$element = this.$element;
+
+        this.$element.trigger("insertedVideo.redactor", [callbackData]);
+      };
+
+      this.insertImage = function(data) {
+        console.log(thisApp.app);
+        var src,
+          $currentTag = thisApp.app.selection.$currentTag,
+          $img = $("<img>"),
+          $figure = $("<figure>"),
+          mediaOption = data.MediaOption,
+          callbackData = {};
+
+        src = mediaOption.URL.replace(/image\..+\./, "image.");
+
+        $img.attr({
+          src: src,
+          alt: mediaOption.Description
+        });
+        $figure.append($img);
+
+        if ($currentTag) {
+          $currentTag.after($figure);
+        } else {
+          this.$editor.prepend($figure);
+        }
+
+        this.image.setEditable($img);
+        this.code.sync();
+
+        // trigger insertedVideo.redactor event after inserted images
+        callbackData.description = mediaOption.Description;
+        callbackData.$img = $figure;
+        callbackData.$editor = this.$editor;
+        callbackData.$element = this.$element;
+
+        this.$element.trigger("insertedImage.redactor", [callbackData]);
+      };
+    }
+  });
+})(window.Redactor);
