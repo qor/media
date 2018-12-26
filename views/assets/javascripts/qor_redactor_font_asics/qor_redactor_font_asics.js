364dcd5e9d97f70712fd0ea54c435e9879f7f72b
wtser <wangtieshou@gmail.com>
2018-12-26 11:20:38 +0800

    Add font-asics plugin

---
+++ views/assets/javascripts/qor_redactor_font_asics/qor_redactor_font_asics.js
@@ -0,0 +1,52 @@
+(function($R) {
+  $R.add("plugin", "font", {
+    translations: {
+      en: {
+        font: "Font",
+        "font-clear": "Clear Font",
+        "font-asics": "ASICS"
+      }
+    },
+    init: function(app) {
+      this.app = app;
+      this.opts = app.opts;
+      this.lang = app.lang;
+      this.toolbar = app.toolbar;
+      this.inline = app.inline;
+    },
+    // public
+    start: function() {
+      var dropdown = {
+        asics: {
+          title: this.lang.get("font-asics"),
+          api: "plugin.font.asics"
+        },
+        clear: {
+          title: this.lang.get("font-clear"),
+          api: "plugin.font.clear"
+        }
+      };
+
+      var $button = this.toolbar.addButton("font", {
+        title: this.lang.get("font")
+      });
+      $button.setIcon('<i class="re-icon-fontfamily"></i>');
+      $button.setDropdown(dropdown);
+    },
+
+    asics: function() {
+      this.clear();
+
+      var args = {
+        tag: "span",
+        class: `font-asics`,
+        type: "toggle"
+      };
+
+      this.inline.format(args);
+    },
+    clear: function() {
+      this.inline.remove({ class: `font-asics` });
+    }
+  });
+})(Redactor);
