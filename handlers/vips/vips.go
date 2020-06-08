7d7b954a12bf10cbb96fdae38d0d03d15fa8aa13
Azuma Fan <azuma@theplant.jp>
2020-06-08 17:48:43 +0800

    use vips to crop/compress image

--- handlers/vips/vips.go
+++ handlers/vips/vips.go
@@ -4,9 +4,20 @@ import (
 	"bytes"
 	"io"
 
+	"path"
+	"strings"
+
 	"github.com/qor/media"
-	"gopkg.in/h2non/bimg.v1"
+	"github.com/theplant/bimg"
 )
+
+var (
+	enableGenerateWebp = false
+)
+
+type Config struct {
+	EnableGenerateWebp bool
+}
 
 type bimgImageHandler struct{}
 
@@ -15,79 +26,113 @@ func (bimgImageHandler) CouldHandle(media media.Media) bool {
 }
 
 func (bimgImageHandler) Handle(media media.Media, file media.FileInterface, option *media.Option) (err error) {
-	// Save Original Image
-	if err = media.Store(media.URL("original"), option, file); err == nil {
-		file.Seek(0, 0)
-
-		// Crop & Resize
-		var buffer bytes.Buffer
-		if _, err := io.Copy(&buffer, file); err != nil {
-			return err
-		}
-
-		img := bimg.NewImage(buffer.Bytes())
-
-		// Handle original image
-		{
-			bimgOption := bimg.Options{Interlace: true}
-
-			// Crop original image if specified
-			if cropOption := media.GetCropOption("original"); cropOption != nil {
-				bimgOption.Top = cropOption.Min.Y
-				bimgOption.Left = cropOption.Min.X
-				bimgOption.AreaWidth = cropOption.Max.X - cropOption.Min.X
-				bimgOption.AreaHeight = cropOption.Max.Y - cropOption.Min.Y
-			}
-
-			// Process & Save original image
-			if buf, err := img.Process(bimgOption); err == nil {
-				media.Store(media.URL(), option, bytes.NewReader(buf))
-			} else {
-				return err
-			}
-		}
-
-		// Handle size images
-		for key, size := range media.GetSizes() {
-			img := bimg.NewImage(buffer.Bytes())
-
-			bimgOption := bimg.Options{
-				Interlace: true,
-			}
-
-			if cropOption := media.GetCropOption(key); cropOption != nil {
-				bimgOption.Top = cropOption.Min.Y
-				bimgOption.Left = cropOption.Min.X
-				bimgOption.AreaWidth = cropOption.Max.X - cropOption.Min.X
-				bimgOption.AreaHeight = cropOption.Max.Y - cropOption.Min.Y
-				bimgOption.Crop = true
-				bimgOption.Force = true
-			}
-
-			// Process & Save size image
-			if _, err := img.Process(bimgOption); err == nil {
-				if buf, err := img.Process(bimg.Options{
-					Interlace: true,
-					Width:     size.Width,
-					Height:    size.Height,
-					Crop:      true,
-					Enlarge:   true,
-					Force:     true,
-				}); err == nil {
-					media.Store(media.URL(key), option, bytes.NewReader(buf))
-				} else {
-					return err
-				}
-			} else {
-				return err
-			}
-		}
-		return nil
+	// Crop & Resize
+	var buffer bytes.Buffer
+	if _, err := io.Copy(&buffer, file); err != nil {
+		return err
 	}
 
-	return err
+	// Save Original Image
+	{
+		img := copyImage(buffer.Bytes())
+		bimgOption := bimg.Options{Lossless: true, Palette: true}
+		// Process & Save original image
+		if buf, err := img.Process(bimgOption); err == nil {
+			media.Store(media.URL("original"), option, bytes.NewReader(buf))
+		} else {
+			return err
+		}
+		if err = generateWebp(media, option, bimgOption, buffer.Bytes(), "original"); err != nil {
+			return err
+		}
+	}
+
+	// Handle default image
+	{
+		img := copyImage(buffer.Bytes())
+		bimgOption := bimg.Options{Palette: true}
+
+		// Crop original image if specified
+		if cropOption := media.GetCropOption("original"); cropOption != nil {
+			bimgOption.Top = cropOption.Min.Y
+			bimgOption.Left = cropOption.Min.X
+			bimgOption.AreaWidth = cropOption.Max.X - cropOption.Min.X
+			bimgOption.AreaHeight = cropOption.Max.Y - cropOption.Min.Y
+		}
+
+		if buf, err := img.Process(bimgOption); err == nil {
+			if err = media.Store(media.URL(), option, bytes.NewReader(buf)); err != nil {
+				return err
+			}
+			if err = generateWebp(media, option, bimgOption, buf); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+
+	// Handle size images
+	for key, size := range media.GetSizes() {
+		if key == "original" {
+			continue
+		}
+		img := copyImage(buffer.Bytes())
+		//bimgOption :=  bimg.Options{}
+		if cropOption := media.GetCropOption(key); cropOption != nil {
+			if _, err := img.Extract(cropOption.Min.Y, cropOption.Min.X, cropOption.Max.X-cropOption.Min.X, cropOption.Max.Y-cropOption.Min.Y); err != nil {
+				return err
+			}
+		}
+		// Process & Save size image
+		if buf, err := img.Process(bimg.Options{
+			Width:   size.Width,
+			Height:  size.Height,
+			Palette: true,
+			Enlarge: true,
+		}); err == nil {
+			if err = media.Store(media.URL(key), option, bytes.NewReader(buf)); err != nil {
+				return err
+			}
+			if err = generateWebp(media, option, bimg.Options{}, buf, key); err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	return
 }
 
-func init() {
+func generateWebp(media media.Media, option *media.Option, bimgOption bimg.Options, buffer []byte, size ...string) (err error) {
+	if !enableGenerateWebp {
+		return
+	}
+	img := copyImage(buffer)
+	bimgOption.Type = bimg.WEBP
+	if buf, err := img.Process(bimgOption); err == nil {
+		url := media.URL(size...)
+		ext := path.Ext(url)
+		extArr := strings.Split(ext, "?")
+		i := strings.LastIndex(url, ext)
+		webpUrl := url[:i] + strings.Replace(url[i:], extArr[0], ".webp", 1)
+		media.Store(webpUrl, option, bytes.NewReader(buf))
+	} else {
+		return err
+	}
+	return
+}
+
+func copyImage(buffer []byte) (img *bimg.Image) {
+	bs := make([]byte, len(buffer))
+	copy(bs, buffer)
+	img = bimg.NewImage(bs)
+	return
+}
+
+func UseVips(cfg Config) {
+	if cfg.EnableGenerateWebp {
+		enableGenerateWebp = true
+	}
 	media.RegisterMediaHandler("image_handler", bimgImageHandler{})
 }
